638a3408ea7a56eadefa3d2073f6afaf6aeca7a9
Irene-nandy <irenekasiva75@gmail.com>
2020-07-17 15:33:26 +0300

    update function dateakan

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 function day (cc,yy,mm,dd){
     return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
  }
+ function dateakan() {
+    var cc = (document.getElementById("CC").value); 
+    var yy = (document.getElementById("YY").value);
+    var mm = (document.getElementById("MM").value);
+    var dd = (document.getElementById("DD").value);
+    var gender=(document.getElementById("Id5").value);
