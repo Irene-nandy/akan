e8893185540f7240bb7d1d68e9ecebb69b9fe8a5
Irene-nandy <irenekasiva75@gmail.com>
2020-07-19 17:42:59 +0300

    add text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function day (cc,yy,mm,dd){
     return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
  }
- function dateakan() {
+ function akandate() {
     var cc = (document.getElementById("CC").value); 
     var yy = (document.getElementById("YY").value);
     var mm = (document.getElementById("MM").value);
